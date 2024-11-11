907cef70305bcad9d72b27882377e289b33fb817
bautistacarbajales <148803605+bautistacarbajales@users.noreply.github.com>
2024-11-11 20:22:30 -0300

    Theme Dark & White

--- script.js
+++ script.js
@@ -241,11 +241,8 @@ function mostrarSubrubros(subrubros, rubro) {
   });
 }
 
-
 //Only testing.
 //Busqueda por input.
-
-
 document.querySelector('.form-control[data-search]').addEventListener('input', function() {
   const searchTerm = this.value.toLowerCase();
   const tableRows = document.querySelectorAll('#tabla-body tr');
@@ -261,6 +258,23 @@ document.querySelector('.form-control[data-search]').addEventListener('input', f
     }
   });
 });
+
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+  body.classList.toggle('light-mode');
+  if (body.classList.contains('light-mode')) {
+    themeIcon.src = 'bin/LazarilloIconBlack.png';
+    
+  } else {
+    themeIcon.src = 'bin/LazarilloIconWhite.png';
+  }
+});
+
+
+
 
 /*
 document.querySelector('.form-control[data-search]').addEventListener('input', function() {
